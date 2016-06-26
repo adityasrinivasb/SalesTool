f4a74629a8b505eee687f42955d4f9a1ca360f4e
Administrator <Administrator@scsbwin-606497.ITTPA.EBIZ.VERIZON.COM>
2016-06-26 11:20:56 +0530

    Initial commit

---
+++ Testing/src/com/test/Main.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		displayGreeting();
+		data.display();
+
+	}
+	
+	private static void displayGreeting() {
+		System.out.println("Hello Happyu Sales People");
+		System.out.println("This App shows sales data");
+	}
+
+}
